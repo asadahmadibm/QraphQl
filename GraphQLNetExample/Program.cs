7bc3b72de1a431ef050056148fdca873fc5e1f9f
Asad Ahmadi <asad.ahmadi.bm@gmail.com>
2023-11-08 22:23:38 +0330

    ini

---
+++ GraphQLNetExample/Program.cs
@@ -0,0 +1,40 @@
+using GraphQL.MicrosoftDI;
+using GraphQL.Server;
+using GraphQL.Types;
+using GraphQLNetExample.Notes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// add notes schema
+builder.Services.AddSingleton<ISchema, NotesSchema>(services => new NotesSchema(new SelfActivatingServiceProvider(services)));
+// register graphQL
+builder.Services.AddGraphQL(options =>
+{
+    options.EnableMetrics = true;
+})
+.AddSystemTextJson();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseGraphQLAltair();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseGraphQL<ISchema>();
+
+app.Run();
